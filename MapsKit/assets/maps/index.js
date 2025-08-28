a2f09e7f14a53d2e73d17a50f8aaa0f2d716f80e
MperorPenguin <matthewmarais14@Gmail.com>
2025-08-28 11:32:01 +0100

    image changes

--- MapsKit/assets/maps/index.js
+++ MapsKit/assets/maps/index.js
@@ -1,5 +1,10 @@
 window.__MAPS__ = [
   {
+    "id": "medievalvillage",
+    "name": "MedievalVillage",
+    "src": "MapsKit/assets/maps/MedievalVillage.png"
+  },
+  {
     "id": "worldglobe",
     "name": "Worldglobe",
     "src": "MapsKit/assets/maps/worldglobe.png"
